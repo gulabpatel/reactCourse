1149a955084d2851c852c4a95f69d9b37b6985c9
gulabpatel <gulabpatel923@gmail.com>
2023-02-07 10:24:23 +0530

    bcrypt salt jwt created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require('../models/User');
 const {body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "Harryisagoodboy";
 
 //Create a User using: POST "/api/auth/createuser". Doesn't require Auth
 router.post('/createuser',[
@@ -21,12 +24,25 @@ router.post('/createuser',[
     if(user){
         return res.status(400).json({errors: "sorry a user with this email already exists"})
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    //create a new user
     user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email
-     })
-     res.json({user})
+     });
+     
+     const data = {
+        user:{
+            id:user.id
+        }
+     }
+     const authtoken = jwt.sign(data, JWT_SECRET);
+     console.log(authtoken)
+    //  res.json({user})
+    res.json({authtoken: authtoken})
     }
      catch (error) {
         console.log(error.message);
