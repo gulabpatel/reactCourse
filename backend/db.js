180b3a2e4915a7d5514b0c64f856aaf7c899cb58
gulabpatel <gulabpatel923@gmail.com>
2023-02-07 09:07:43 +0530

    password created

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2"
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2"
 mongoose.set('strictQuery', false);
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, (err)=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,47 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const {body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
+//Create a User using: POST "/api/auth/createuser". Doesn't require Auth
+router.post('/createuser',[
+    body('name', 'Enter a valid name').isLength({min:3}),
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password must be at least five characters').isLength({min:5}),
+], async(req,res)=>{
+    const errors = validationResult(req);
+    //If there are errors, return Bad request and errors
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    //check whether the user with this email exists already
+    try {
+    let user = await User.findOne({email: req.body.email});
+    if(user){
+        return res.status(400).json({errors: "sorry a user with this email already exists"})
+    }
+    user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+     })
+     res.json({user})
+    }
+     catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some error occured")
+     }
 
-//Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', async(req,res)=>{
-    console.log(req.body)
-    const user = User(req.body)
-    await user.save()
-    res.send(req.body)
+     //there can be others errors not only dplicate email
+    //.then(user => res.json(user))
+    // .catch(err => {console.log(err),
+    // res.json({error: 'Please enter a unique Email value', message: err.message})})
+    
+    //simplest way to create and save data in mongodb
+    // console.log(req.body)
+    // const user = User(req.body)
+    // await user.save()
 
 })
 
